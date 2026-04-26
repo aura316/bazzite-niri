d338500a3a8c76ff7255afd4e6169a91e6e22f17
aura316 <minepullbear@gmail.com>
2026-04-26 12:51:09 +0100

    Added niri and noctalia

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,8 +10,10 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+# dnf5 install -y tmux 
 
+# Only available in fedora 44
+# dnf5 -y install cliphist 
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
@@ -19,6 +21,16 @@ dnf5 install -y tmux
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
+dnf5 -y copr enable yalter/niri
+dnf5 -y install niri
+dnf5 -y copr disable yalter/niri
+
+# Fetch noctalia from terra
+dnf5 config-manager setopt terra.enabled=1 terra-extras.enabled=1
+dnf5 -y install noctalia-shell
+dnf5 config-manager setopt terra.enabled=0 terra-extras.enabled=0
+
+
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+# systemctl enable podman.socket
