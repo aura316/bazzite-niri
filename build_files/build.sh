0d971592e016938d1c071ab1688be1dc1bb91d4e
Aura <minepullbear@gmail.com>
2026-04-27 23:04:40 +0100

    Add zsh and plugins

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,7 +9,9 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-dnf5 install -y cascadia-code-fonts tmux neovim fzf fd-find bat eza
+dnf5 install -y cascadia-code-fonts 
+dnf5 install -y tmux neovim fzf fd-find bat eza 
+dnf5 install -y zsh zsh-autosuggestions zsh-syntax-highlighting
 
 # Only available in fedora 44
 # dnf5 -y install cliphist 
